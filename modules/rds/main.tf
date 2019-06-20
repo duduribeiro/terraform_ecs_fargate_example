13c2838a648f3f031b862344a0bb6b4431e8e03d
Martin Cerny <cerny.mrtn@gmail.com>
2019-06-20 13:56:18 +0100

    Remove snapshot identifier from rds (#8)

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -71,7 +71,7 @@ resource "aws_db_instance" "rds" {
   db_subnet_group_name   = "${aws_db_subnet_group.rds_subnet_group.id}"
   vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
   skip_final_snapshot    = true
-  snapshot_identifier    = "rds-${var.environment}-snapshot"
+  #snapshot_identifier    = "rds-${var.environment}-snapshot"
   tags {
     Environment = "${var.environment}"
   }
